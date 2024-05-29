183c745555186ce5d4f69bf97c2bf3691ab5cac5
jordi <nkwamojordi@yahoo.com>
2024-05-29 00:20:15 -0400

    new add again

---
+++ build/precache-manifest.adf934fa831135d701da2fabe0b69ee9.js
@@ -0,0 +1,290 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "5e936756ff1440ffdccff49ffb0be1d7",
+    url: "/jordinkwamo/index.html",
+  },
+  {
+    revision: "66c1288fd71d3de1e117",
+    url: "/jordinkwamo/static/css/main.319e74a5.chunk.css",
+  },
+  {
+    revision: "41651245ef96b99332d9",
+    url: "/jordinkwamo/static/js/2.70292fef.chunk.js",
+  },
+  {
+    revision: "66c1288fd71d3de1e117",
+    url: "/jordinkwamo/static/js/main.ebeef41d.chunk.js",
+  },
+  {
+    revision: "a4b7313ff4cd5477818a",
+    url: "/jordinkwamo/static/js/runtime-main.d2bb5e38.js",
+  },
+  {
+    revision: "21f233e19402cc4a66866a7f31191f0d",
+    url: "/jordinkwamo/static/media/Agustina.21f233e1.woff",
+  },
+  {
+    revision: "4457817ac2b9993c65e81aa05828fe9c",
+    url: "/jordinkwamo/static/media/GoogleSans-Bold.4457817a.ttf",
+  },
+  {
+    revision: "90773b6158663ab0fe78b32680733677",
+    url: "/jordinkwamo/static/media/GoogleSans-BoldItalic.90773b61.ttf",
+  },
+  {
+    revision: "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    url: "/jordinkwamo/static/media/GoogleSans-Italic.0ecddcde.ttf",
+  },
+  {
+    revision: "8d57e4014b18edef070d285746485115",
+    url: "/jordinkwamo/static/media/GoogleSans-Medium.8d57e401.ttf",
+  },
+  {
+    revision: "8fd3737925e83c87d78a13700ccf9a62",
+    url: "/jordinkwamo/static/media/GoogleSans-MediumItalic.8fd37379.ttf",
+  },
+  {
+    revision: "b5c77a6aed75cdad9489effd0d5ea411",
+    url: "/jordinkwamo/static/media/GoogleSans-Regular.b5c77a6a.ttf",
+  },
+  {
+    revision: "ee6539921d713482b8ccd4d0d23961bb",
+    url: "/jordinkwamo/static/media/Montserrat-Regular.ee653992.ttf",
+  },
+  {
+    revision: "16559172ca1d4d552390b48f19e102ba",
+    url: "/jordinkwamo/static/media/address_image.16559172.svg",
+  },
+  {
+    revision: "dcd6dee2bd7458a4a576dc131506fd83",
+    url: "/jordinkwamo/static/media/animated_logo.dcd6dee2.svg",
+  },
+  {
+    revision: "058eca4cbdc03a1dad9a6bb943ac7831",
+    url: "/jordinkwamo/static/media/blogs_image.058eca4c.svg",
+  },
+  {
+    revision: "95dbaf00afdd113ae8ff0131b2378998",
+    url: "/jordinkwamo/static/media/cite_collegiale_logo.95dbaf00.jpeg",
+  },
+  {
+    revision: "cacbc42b661f39344af81c631d8d36a9",
+    url: "/jordinkwamo/static/media/cloud_infrastructure.cacbc42b.svg",
+  },
+  {
+    revision: "fee92c18b1cfbd1faa704c51047f113d",
+    url: "/jordinkwamo/static/media/codeInLogo.fee92c18.png",
+  },
+  {
+    revision: "5f915405511f649dad433a6db9adbff4",
+    url: "/jordinkwamo/static/media/contactMail.5f915405.png",
+  },
+  {
+    revision: "ebf35d7f33ebbd4a4c6cff7e66e21fdb",
+    url: "/jordinkwamo/static/media/data_science.ebf35d7f.svg",
+  },
+  {
+    revision: "75a7107b7739a46950f66de29779eb52",
+    url: "/jordinkwamo/static/media/deeplearning_ai_logo.75a7107b.png",
+  },
+  {
+    revision: "3e23ca14e7a7c8a7b63d255466749000",
+    url: "/jordinkwamo/static/media/developerActivity.3e23ca14.svg",
+  },
+  {
+    revision: "63f707f224c8a842ac928c36e3be879a",
+    url: "/jordinkwamo/static/media/dsc_logo.63f707f2.png",
+  },
+  {
+    revision: "3bab7a9dc4324f1b2364d25a47fcfcf0",
+    url: "/jordinkwamo/static/media/education.3bab7a9d.svg",
+  },
+  {
+    revision: "a00ede3f70759480e81f14f203edaa51",
+    url: "/jordinkwamo/static/media/experience.a00ede3f.svg",
+  },
+  {
+    revision: "0017cc6de3f72f67753efa37bb796fbd",
+    url: "/jordinkwamo/static/media/fa-brands-400.0017cc6d.svg",
+  },
+  {
+    revision: "06147b6cd88c7346cecd1edd060cd5de",
+    url: "/jordinkwamo/static/media/fa-brands-400.06147b6c.ttf",
+  },
+  {
+    revision: "5063b105c7646c8043d58c5289f02cca",
+    url: "/jordinkwamo/static/media/fa-brands-400.5063b105.eot",
+  },
+  {
+    revision: "c5e0f14f88a828261ba01558ce2bf26f",
+    url: "/jordinkwamo/static/media/fa-brands-400.c5e0f14f.woff",
+  },
+  {
+    revision: "cccc9d29470e879e40eb70249d9a2705",
+    url: "/jordinkwamo/static/media/fa-brands-400.cccc9d29.woff2",
+  },
+  {
+    revision: "65b286af947c0d982ca01b40e1fcab38",
+    url: "/jordinkwamo/static/media/fa-regular-400.65b286af.ttf",
+  },
+  {
+    revision: "c1a866ec0e04a5e1915b41fcf261457c",
+    url: "/jordinkwamo/static/media/fa-regular-400.c1a866ec.eot",
+  },
+  {
+    revision: "c4f508e7c4f01a9eeba7f08155cde04e",
+    url: "/jordinkwamo/static/media/fa-regular-400.c4f508e7.woff",
+  },
+  {
+    revision: "e647ddbb6d95cb01deac07ffafb2a085",
+    url: "/jordinkwamo/static/media/fa-regular-400.e647ddbb.svg",
+  },
+  {
+    revision: "f5f2566b93e89391da4db79462b8078b",
+    url: "/jordinkwamo/static/media/fa-regular-400.f5f2566b.woff2",
+  },
+  {
+    revision: "0bff33a5fd7ec390235476b4859747a0",
+    url: "/jordinkwamo/static/media/fa-solid-900.0bff33a5.ttf",
+  },
+  {
+    revision: "333bae208dc363746961b234ff6c2500",
+    url: "/jordinkwamo/static/media/fa-solid-900.333bae20.woff",
+  },
+  {
+    revision: "424c59fb1df79fcc3d0a41909c0192d2",
+    url: "/jordinkwamo/static/media/fa-solid-900.424c59fb.svg",
+  },
+  {
+    revision: "44d537ab79f921fde5a28b2c1636f397",
+    url: "/jordinkwamo/static/media/fa-solid-900.44d537ab.woff2",
+  },
+  {
+    revision: "8e4a6dcc692b3887f9f542cd6894d6d4",
+    url: "/jordinkwamo/static/media/fa-solid-900.8e4a6dcc.eot",
+  },
+  {
+    revision: "2936ce95f550e3feef7230ea543a4b60",
+    url: "/jordinkwamo/static/media/feelingProud.2936ce95.svg",
+  },
+  {
+    revision: "fce6080726685cabdb4f75a0d8018325",
+    url: "/jordinkwamo/static/media/fullstack.fce60807.svg",
+  },
+  {
+    revision: "0ee7e8c9444cdfefc10f6d86e18b9b5b",
+    url: "/jordinkwamo/static/media/gcp_logo.0ee7e8c9.png",
+  },
+  {
+    revision: "6eeb2e810d0fd9f3ca2dcd72de228e68",
+    url: "/jordinkwamo/static/media/github_logo.6eeb2e81.png",
+  },
+  {
+    revision: "0c8a4c114ad00f7854e91550379b9b91",
+    url: "/jordinkwamo/static/media/googleAssistant.0c8a4c11.jpg",
+  },
+  {
+    revision: "a6addc3fb6605480614b992ede8cd434",
+    url: "/jordinkwamo/static/media/googleAssistant.a6addc3f.svg",
+  },
+  {
+    revision: "d3fccbe1db76b96f4f320d9c0f7da30c",
+    url: "/jordinkwamo/static/media/ibm_logo.d3fccbe1.png",
+  },
+  {
+    revision: "b9afe2f6f2885841eebc34e9bfc9d5b6",
+    url: "/jordinkwamo/static/media/iga_logo.b9afe2f6.png",
+  },
+  {
+    revision: "571a8584e69ecfbe326c8b8385fbbf54",
+    url: "/jordinkwamo/static/media/iiitk_logo.571a8584.png",
+  },
+  {
+    revision: "29152cf756a1facead5393c32e373371",
+    url: "/jordinkwamo/static/media/intel_logo.29152cf7.jpg",
+  },
+  {
+    revision: "60ed0bd3f8b99ac1d29e74575f804aaa",
+    url: "/jordinkwamo/static/media/iu_logo.60ed0bd3.png",
+  },
+  {
+    revision: "5b4a6449747ec61a6d9aa874f2a9ceba",
+    url: "/jordinkwamo/static/media/jsFramework.5b4a6449.svg",
+  },
+  {
+    revision: "c597fbded5de799dfb9abc3fc4f1c26f",
+    url: "/jordinkwamo/static/media/legato_logo.c597fbde.png",
+  },
+  {
+    revision: "a0832f978cadf9f8beff07c360a3524e",
+    url: "/jordinkwamo/static/media/letservu_logo.a0832f97.png",
+  },
+  {
+    revision: "83ac24fb968a474c6a63f7a4e9a8f62c",
+    url: "/jordinkwamo/static/media/manOnTable.83ac24fb.svg",
+  },
+  {
+    revision: "069938a84edd6ebf383c318e9feb5d4c",
+    url: "/jordinkwamo/static/media/muffito_logo.069938a8.png",
+  },
+  {
+    revision: "95a12a2b8c3149cce2175679755c4b4d",
+    url: "/jordinkwamo/static/media/nptel_logo.95a12a2b.png",
+  },
+  {
+    revision: "414dfbd98516b39255a3e22ed90b3f7d",
+    url: "/jordinkwamo/static/media/portfolio.414dfbd9.gif",
+  },
+  {
+    revision: "5faf790badc18fa9030f94836ece1df1",
+    url: "/jordinkwamo/static/media/programmer.5faf790b.svg",
+  },
+  {
+    revision: "b8ba948796d7ab532673c5ed2f315e74",
+    url: "/jordinkwamo/static/media/projects_image.b8ba9487.svg",
+  },
+  {
+    revision: "71408db71465afd742233e4a96f06526",
+    url: "/jordinkwamo/static/media/pwa.71408db7.png",
+  },
+  {
+    revision: "2c497738be3831492aeb6bdc43f19154",
+    url: "/jordinkwamo/static/media/stanford_logo.2c497738.png",
+  },
+  {
+    revision: "6ac91b161c2c9934a44f72d75d18142d",
+    url: "/jordinkwamo/static/media/talksCardBack.6ac91b16.svg",
+  },
+  {
+    revision: "3fa5424232370e3d049c9555d9c440a4",
+    url: "/jordinkwamo/static/media/talksCardBackAlt.3fa54242.svg",
+  },
+  {
+    revision: "0993e3246bf5146154391a0afef79b03",
+    url: "/jordinkwamo/static/media/tiktok_logo.0993e324.png",
+  },
+  {
+    revision: "d63630893eb8cb64af65f395d393c481",
+    url: "/jordinkwamo/static/media/ui_ux_design.d6363089.svg",
+  },
+  {
+    revision: "46b6b9a1e3be92f9e4354344cc5ee2d6",
+    url: "/jordinkwamo/static/media/undraw_certificate_re_yadi.46b6b9a1.svg",
+  },
+  {
+    revision: "3d02522ece60fc77ccb8fb1d26ad0ee8",
+    url: "/jordinkwamo/static/media/undraw_code_review_re_woeb.3d02522e.svg",
+  },
+  {
+    revision: "c60f9808e3df8e08fadfd7ebee580210",
+    url: "/jordinkwamo/static/media/undraw_done_re_oak4.c60f9808.svg",
+  },
+  {
+    revision: "690f61f6e5cb0dbd8278f570e1b58aec",
+    url: "/jordinkwamo/static/media/undraw_male_avatar_g98d.690f61f6.svg",
+  },
+  {
+    revision: "3d049af9ebda363f46c941c772045bca",
+    url: "/jordinkwamo/static/media/undraw_programming_re_kg9v.3d049af9.svg",
+  },
+]);
